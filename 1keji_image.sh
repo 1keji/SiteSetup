a5a7c14f99bf2dd9a69fa34a68a63cd05a4b78ac
1keji <vi66999@icloud.com>
2025-01-23 08:33:56 +0800

    Add files via upload

--- 1keji_image.sh
+++ 1keji_image.sh
@@ -1,12 +1,9 @@
-# 显示提示信息
-def display_message():
-    print("====================================")
-    print("此脚本仅提供给本频道的粉丝朋友使用。")
-    print("加入我们的频道以获取更多信息！")
-    print("频道链接: https://www.youtube.com/@1keji_net")
-    print("Telegram 群: https://t.me/tg_1keji")
-    print("====================================")
+#!/bin/bash
 
-# 调用提示信息
-if __name__ == "__main__":
-    display_message()
+# 输出提示信息
+echo "===================================="
+echo "此脚本仅提供给本频道的粉丝朋友使用。"
+echo "加入我们的频道以获取更多信息！"
+echo "频道链接: https://www.youtube.com/@1keji_net"
+echo "Telegram 群: https://t.me/tg_1keji"
+echo "===================================="
